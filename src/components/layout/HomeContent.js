40ef2a73f299eb2b9cafe872a1b888a87687e22b
ShwetaNadar <shwetanadar99999@gmail.com>
2021-06-08 19:40:11 +0530

    fetch from youtube api

--- src/components/layout/HomeContent.js
+++ src/components/layout/HomeContent.js
@@ -1,7 +1,19 @@
+import { useEffect } from "react";
 import ChipsContainer from "./ChipsContainer";
 import VideosContainer from "./VideosContainer";
 
 const HomeContent = (props) => {
+  useEffect(() => {
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=9&regionCode=PK&key="
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.items);
+      });
+  }, []);
   return (
     <div>
       <ChipsContainer />
