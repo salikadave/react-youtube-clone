3509c5754b9efd56b0339f3093f86f42945226b2
ShwetaNadar <shwetanadar99999@gmail.com>
2021-06-09 21:18:06 +0530

    add watchpage queue

--- src/pages/VideoPage.js
+++ src/pages/VideoPage.js
@@ -2,17 +2,39 @@ import { useParams } from "react-router-dom";
 
 import VideoPlayer from "../components/UI/VideoPlayer";
 import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
+// import CardContent from "@material-ui/core/CardContent";
+// import CardMedia from "@material-ui/core/CardMedia";
+import CardHeader from "@material-ui/core/CardHeader";
 import Typography from "@material-ui/core/Typography";
+import QueueCard from "../components/UI/QueueCard";
 
 const VideoPage = () => {
   const { id } = useParams();
   console.log(id);
-
+  const videoDetails = {
+    title: "title",
+    channelName: "channelName",
+    thumbnail: "thumbnail",
+  };
   return (
-    <div style={{ padding: 40, marginTop: 50, display: "flex" }}>
-      <VideoPlayer videoId={id} />
+    <div style={{ display: "flex" }}>
+      <div style={{ padding: 40, marginTop: 50 }}>
+        <VideoPlayer videoId={id} />
+      </div>
+      <Card
+        style={{
+          height: "auto",
+          width: 500,
+          // backgroundColor: "#eee",
+          marginTop: 90,
+        }}
+      >
+        <CardHeader
+          title={<Typography style={{ fontWeight: "bold" }}>Queue</Typography>}
+          subheader="1/3"
+        />
+        <QueueCard video={videoDetails} />
+      </Card>
     </div>
   );
 };
